870ee4507dc3b66ddeb3e16c114ad0234366d309
S Mautner <mautner@cs.uni-freiburg.de>
2018-09-18 10:45:23 +0100

    hayaku!

--- runall.sh
+++ runall.sh
@@ -1,3 +1,3 @@
 rm ".res/"*
 rm -r ".log/1"
-echo -e (seq 0 0)" "(seq 0 3)" "(seq 0 3)" "(seq 0 9)"\n" | parallel --bar --results .log -j 15 python3 reconstruct.py
+echo -e (seq 0 3)" "(seq 0 3)" "(seq 0 0)" "(seq 0 9)"\n" | parallel --bar --results .log -j 36 python3 reconstruct.py
