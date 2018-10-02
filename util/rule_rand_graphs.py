7e47ba2592e3425ff6e968cca0ea9a378d6a4454
S Mautner <mautner@cs.uni-freiburg.de>
2018-10-02 23:03:29 +0200

    hayaku!

--- util/rule_rand_graphs.py
+++ util/rule_rand_graphs.py
@@ -1,14 +1,20 @@
+import random
 
 
 
+import graphlearn3.util.util as u
+from exploration.pareto  import MYOPTIMIZER as myop
 
 
 
 import graphlearn3.lsgg as lsgg
 def rule_rand_graphs(input_set, numgr =100, iter= 1):
 
+
+
+    # make grammar, fit on input
     grammar = lsgg.lsgg(decomposition_args={"radius_list": [0, 1],
-                                     "thickness_list": [ 2],
+                                     "thickness_list": [ 1],
                                      'hash_bitmask': lsgg._hash_bitmask_},
                  filter_args={"min_cip_count": 1,
                               "min_interface_count": 2},
@@ -16,21 +22,19 @@ def rule_rand_graphs(input_set, numgr =100, iter= 1):
                  half_step_distance= False )
     grammar.fit(input_set)
 
-    import graphlearn3.util as u
-    u.draw_grammar_term(grammar)
+    cleaner = myop()
+    list(cleaner.duplicate_rm(input_set)) # makes sure that we never ouitput the input. dup_rm saves all hashes
+
     for i in range(iter):
-        input_set = [g for start in input_set for g  in grammar.neighbors(start)][:numgr]
+        input_set = [g for start in input_set for g  in grammar.neighbors(start)]
+        input_set = cleaner.duplicate_rm(input_set)
+        random.shuffle(input_set)
+        input_set= input_set[:numgr]
 
 
     # also needs duplicate removal
 
-    return input_set
-
-
-
-
-
-
+    return input_set, grammar
 
 
 
@@ -41,7 +45,27 @@ def rule_rand_graphs(input_set, numgr =100, iter= 1):
 def test_rulerand():
     import util.random_graphs as rg
     import structout as so
-    grs  = rg.make_graphs_static()[:10] # default returns 100 graphs..
-    res =rule_rand_graphs(grs, numgr = 100)
-    res =rule_rand_graphs(res[:10], numgr = 100)
-    so.gprint(res)
+    import graphlearn3.util.setoperations as setop
+    grs  = rg.make_graphs_static()[:20]#[:10] # default returns 100 graphs..
+    res1, grammar1 =rule_rand_graphs(grs, numgr = 500,iter=2)
+    res, grammar2=rule_rand_graphs(res1, numgr = 50, iter=1)
+    #so.gprint(res) #!!!!!!!!
+
+    '''
+    print("initial grammar:")
+    print (grammar1)
+    print("grammar after iteration")
+    print (grammar2)
+    inter = setop.intersect(grammar1, grammar2)
+    print("intersection between grammars:")
+    print (inter)
+    '''
+
+
+    print("grammar1 - grammar2")
+    diff = setop.difference(grammar1,grammar2)
+    u.draw_grammar_term(diff)
+
+
+    #u.draw_grammar_term(unique2)  # !!!!!!!!!!!!!!!!!!!!!!!!!
+    print ("generated %d graphs" % len(res1))
