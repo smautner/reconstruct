6ed3a4ae8fd9006c74376f4e5cbe9ab21a6a6f4c
S Mautner <mautner@cs.uni-freiburg.de>
2018-10-04 18:36:38 +0200

    hayaku!

--- util/rule_rand_graphs.py
+++ util/rule_rand_graphs.py
@@ -14,8 +14,7 @@ def rule_rand_graphs(input_set, numgr =100, iter= 1):
 
     # make grammar, fit on input
     grammar = lsgg.lsgg(decomposition_args={"radius_list": [0, 1],
-                                     "thickness_list": [ 1],
-                                     'hash_bitmask': lsgg._hash_bitmask_},
+                                     "thickness_list": [ 1] },
                  filter_args={"min_cip_count": 1,
                               "min_interface_count": 2},
                  cip_root_all = False,
@@ -46,9 +45,9 @@ def test_rulerand():
     import util.random_graphs as rg
     import structout as so
     import graphlearn3.util.setoperations as setop
-    grs  = rg.make_graphs_static()[:20]#[:10] # default returns 100 graphs..
+    grs  = rg.make_graphs_static()[:30]#[:10] # default returns 100 graphs..
     res1, grammar1 =rule_rand_graphs(grs, numgr = 500,iter=2)
-    res, grammar2=rule_rand_graphs(res1, numgr = 50, iter=1)
+    #res, grammar2=rule_rand_graphs(res1, numgr = 50, iter=1)
     #so.gprint(res) #!!!!!!!!
 
     '''
@@ -62,10 +61,10 @@ def test_rulerand():
     '''
 
 
-    print("grammar1 - grammar2")
-    diff = setop.difference(grammar1,grammar2)
-    u.draw_grammar_term(diff)
+    #print("grammar1 - grammar2")
+    #diff = setop.difference(grammar1,grammar2)
+    #u.draw_grammar_term(diff)
 
 
     #u.draw_grammar_term(unique2)  # !!!!!!!!!!!!!!!!!!!!!!!!!
-    print ("generated %d graphs" % len(res1))
+    #print ("generated %d graphs" % len(res1))
