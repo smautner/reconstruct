345cbc2f29bb02f7ba2f1dba76ac2a5fb0e9bf9c
S Mautner <mautner@cs.uni-freiburg.de>
2018-10-04 17:37:50 +0100

    save

--- exploration/pareto.py
+++ exploration/pareto.py
@@ -300,10 +300,46 @@ class MYOPTIMIZER(object):
         logger.debug("++++++++  NEW OPTI ROUND +++++++")
         costs = self.get_costs(graphs)
         status = self.checkstatus(costs, graphs)
-        graphs = self.filter_by_cost(costs, graphs)
+        graphs = self.filter_by_cost2(costs, graphs)
         graphs = self._expand_neighbors(graphs)
         graphs = self.duplicate_rm(graphs)
         return graphs, status
+
+    def filter_by_cost2(self,costs,graphs):
+        """same as filter_by_cost but treats size as any other filter"""
+        timenow=time.time()
+        in_count = len(graphs)
+        if in_count< 50:
+            logger.debug('cost_filter: keep all %d graphs' % in_count)
+            return graphs
+       
+        # need to keep x best in each category
+        costs_ranked = np.argsort(costs,axis=0)[:self.keeptop]
+        want , counts = np.unique(costs_ranked,return_counts=True) 
+
+        res = [graphs[idd] for idd,count in zip( want,counts) if count > 0 ] 
+        for g,score in zip(res,want):
+            g.graph['history'].append(costs[score])
+
+        # DEBUG TO SHOW THE REAL DISTANCE
+        if self.cheat:
+            print ("real distances for all kept graphs, axis 1 are the estimators that selected them")
+            matrix = np.hstack(
+                        [self.cheater.cheat_calc([graphs[z] for z in costs_ranked[:,i]]).reshape(-1,1)
+                        for i in range(costs_ranked.shape[1])]
+                )
+            print(matrix)
+            print (costs[costs_ranked[:,1],0])
+            print (costs[costs_ranked[:,0],0])
+
+            stuff = np.where(matrix == 0.0)
+            if len(stuff[0])>0:
+                from util import util
+                util.dumpfile(graphs[costs_ranked[stuff][0]],"gr")
+                print ("graph dumped")
+
+        logger.debug('cost_filter: got %d graphs, reduced to %d (%.2fs)'%(in_count,len(res),time.time()-timenow))
+        return res
 
     def filter_by_cost(self,costs,graphs):
 
