74197fc49385914e05d34b5afe533f581a7d5020
S Mautner <mautner@cs.uni-freiburg.de>
2018-11-18 17:22:25 +0000

    inspector kann meer

--- inspecttasks.py
+++ inspecttasks.py
@@ -1,11 +1,24 @@
 import reconstruct as r
-
 graphsets = r.loadfile(".tasks")
-
-
+import numpy as np
 from structout import gprint
+
+
+def size(graphs):
+    return np.array([len(g) for g in graphs]).mean()
+
+def labels(graphs):
+    nlabels =[d["label"] for g in graphs for n,d in g.nodes(data=True)]
+    elabels =[d["label"] for g in graphs for a,b,d in g.edges(data=True)]
+    return (len(set(nlabels)),len(set(elabels)))
+
 for graphs in graphsets:
-    print ("inspectin dataset")
     for i in range(5):
         gprint(graphs[i*5:i*5+5])
 
+    # we should collect stats... somehow..
+    # avg size of whole set vs last 20 
+    print("avg size all:",size(graphs))
+    print("avg size last 20",size(graphs[-20:]))
+    print ("labels all nodelabels/edgelabels:",labels(graphs))
+    print("labels last 20  n/l:",labels(graphs[-20:]))
