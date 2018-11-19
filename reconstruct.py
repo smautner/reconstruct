09afb99420a1094a5584fe2a6173c797f02298e4
S Mautner <mautner@cs.uni-freiburg.de>
2018-11-19 23:35:05 +0000

    hayaku!

--- reconstruct.py
+++ reconstruct.py
@@ -105,8 +105,8 @@ instancemakerparams =maketasks(params_insta)
 params_opt = {
     'keyorder' :  ["half_step_distance",'n_iter','multiproc',"add_grammar_rules","keeptop","squared_error","graph_size_limiter"],
     "half_step_distance" : [True], # true clearly supperior
-    "n_iter":[15,20], # 5 just for ez problems
-    "keeptop":[10,15], # 20 seems enough
+    "n_iter":[15], # 5 just for ez problems
+    "keeptop":[10], # 20 seems enough
     'multiproc': [1],
     "add_grammar_rules":[True],
     "squared_error": [False], # False slightly better 590:572 
