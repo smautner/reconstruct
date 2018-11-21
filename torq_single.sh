807ac8dc62cf84b8fa637ce2e5eb9f7bc9b376e9
S Mautner <mautner@cs.uni-freiburg.de>
2018-11-21 13:54:07 +0100

    rm torq paralllel

--- torq_single.sh
+++ torq_single.sh
@@ -12,6 +12,10 @@
 cd /home/fr/fr_fr/fr_sm1105/code/reconstruct
 
 
-#seq $1 $2 | parallel --results .log -j 20 python3 reconstruct.py 
 python3 reconstruct.py $1
 
+
+
+# this was for parallel stuff.. just chills in the q ... 
+##PBS -l nodes=1:ppn=20,mem=127gb
+#seq $1 $2 | parallel --results .log -j 20 python3 reconstruct.py 
