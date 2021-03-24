2f73c50b2ed3593b6ac8dbb2fd2d4a811cb8e163
mh595@uni-freiburg.de <fr_mh595@login01.binac.uni-tuebingen.de>
2021-03-24 18:54:35 +0100

    New stuff for run script

--- run_in_order.sh
+++ run_in_order.sh
@@ -5,7 +5,8 @@ conda activate binenv
 
 REPEATS=50 ### Change to 100 for Normal or 250 for Chem
 
-execute () {    #### Make sure to change filename in first sed command to 'chem_runall_binac.sh' or just 'runall_binac.sh'
+execute () {    ######## Make sure to change filename in first sed command to 'chem_runall_binac.sh' or just 'runall_binac.sh'
+
     sed '/reconstruct.py/s/$/'"$STRING"'/' runall_binac.sh > .run_$RESPREFIX.sh
     JOBID=$(qsub -q short -t 1-$REPEATS .run_$RESPREFIX.sh | sed -r 's/^[^0-9]*([0-9]+).*$/\1/')
     echo "Current Task: $JOBID : $STRING"
@@ -41,7 +42,7 @@ for PARETO in 'random' 'greedy' 'pareto_only' 'all' 'default'; do
                         for SIZELIMITER in 1; do
                             RESPREFIX="$CIPSELECTOR-$CIPK-$CONTEXTSIZE-$MINCOUNT-$SIZELIMITER-$NORMALIZATION-$DECOMPRADIUS-$PARETO"
                             STRING=" --cipselector_option $CIPSELECTOR --pareto_option $PARETO --cipselector_k $CIPK --context_size $CONTEXTSIZE --min_count $MINCOUNT --graph_size_limiter $SIZELIMITER --use_normalization $NORMALIZATION --max_decompose_radius $DECOMPRADIUS --resprefix $RESPREFIX"
-                            report ##  Replace this with report/execute/pass
+                            ##report ##  Replace this with report/execute/pass
                         done
                     done
                 done
@@ -50,37 +51,64 @@ for PARETO in 'random' 'greedy' 'pareto_only' 'all' 'default'; do
     done
 done
 ## Chemset Comparison
-#for CONTEXTSIZE in 1 2; do
-#    for CIPK in 200 300 400; do
-#        RESPREFIX="cipK-$CIPK-contextsize-$CONTEXTSIZE"
-#        STRING=" --context_size $CONTEXTSIZE --cipselector_k $CIPK --resprefix $RESPREFIX"
-#        pass
-#    done
-#done
-
-## Artificial Comparison
-#for CONTEXTSIZE in 1 2; do
-#    RESPREFIX="coresizes-012-contextsize-$CONTEXTSIZE"
-#    STRING=" --core_sizes 0 1 2 --context_size $CONTEXTSIZE --resprefix $RESPREFIX"
-#    report
-#    RESPREFIX="coresizes-01-contextsize-$CONTEXTSIZE"
-#    STRING=" --core_sizes 0 1 --context_size $CONTEXTSIZE --resprefix $RESPREFIX"
-#    report
-#    RESPREFIX="coresizes-0-contextsize-$CONTEXTSIZE"
-#    STRING=" --core_sizes 0 --context_size $CONTEXTSIZE --resprefix $RESPREFIX"
-#    report
-#done
-
-
-## Cipselector 1: 100 200 400 800
-#for CIPK in 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200; do
-for CIPK in 10 30 50 70 90; do
-    RESPREFIX="cipsel1_$CIPK"
-    STRING=" --cipselector_option 1 --cipselector_k $CIPK --resprefix $RESPREFIX"
-#    pass
+for CONTEXTSIZE in 1 2; do
+    for CIPK in 100 200 300 400; do
+        RESPREFIX="cipK-$CIPK-contextsize-$CONTEXTSIZE"
+        STRING=" --pareto_option 'default' --core_sizes 0 1 2 3 --context_size $CONTEXTSIZE --cipselector_k $CIPK --resprefix $RESPREFIX"
+#        reportchem
+    done
 done
 
-## Cipselector 2: (Default) 1 5 10 15 20 #### REMOVED 100 FOR CHEMSETS
+## Artificial Comparison
+CIPSELECTOR=2
+for CONTEXTSIZE in 1 2; do
+    RESPREFIX="coresizes-012-contextsize-$CONTEXTSIZE"
+    STRING=" --cipselector_option $CIPSELECTOR --core_sizes 0 1 2 --context_size $CONTEXTSIZE --resprefix $RESPREFIX"
+#    report
+    RESPREFIX="coresizes-01-contextsize-$CONTEXTSIZE"
+    STRING=" --cipselector_option $CIPSELECTOR --core_sizes 0 1 --context_size $CONTEXTSIZE --resprefix $RESPREFIX"
+#    report
+    RESPREFIX="coresizes-0-contextsize-$CONTEXTSIZE"
+    STRING=" --cipselector_option $CIPSELECTOR --core_sizes 0 --context_size $CONTEXTSIZE --resprefix $RESPREFIX"
+#    report
+done
+
+## Comparison of Average Productions ##
+    CIPSELECTOR=0
+    CIPK=1000
+    RESPREFIX="cipsel0_$CIPK"
+    STRING=" --cipselector_option $CIPSELECTOR --cipselector_k $CIPK --resprefix $RESPREFIX"
+#    report
+    CIPSELECTOR=1
+    CIPK=100
+    RESPREFIX="cipsel1_$CIPK"
+    STRING=" --cipselector_option $CIPSELECTOR --cipselector_k $CIPK --resprefix $RESPREFIX"
+#    report
+    CIPSELECTOR=2
+    CIPK=10
+    RESPREFIX="cipsel2_$CIPK"
+    STRING=" --cipselector_option $CIPSELECTOR --cipselector_k $CIPK --resprefix $RESPREFIX"
+#    report
+##########################
+
+## Cipselector 1: 100 200 400 800
+for CIPK in 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200; do
+#for CIPK in 10 30 50 70 90; do
+    RESPREFIX="cipsel1_$CIPK"
+    STRING=" --cipselector_option 1 --cipselector_k $CIPK --resprefix $RESPREFIX"
+#    report
+done
+
+for KEEP in 12 30 60; do
+    for CIPK in 50 100 250; do
+        RESPREFIX="keep-$KEEP-cipk-$CIPK"
+        STRING=" --keepgraphs $KEEP --cipselector_option 1 --cipselector_k $CIPK --resprefix $RESPREFIX"
+#        report
+    done
+done
+
+
+## Cipselector 2: 1 5 10 15 20 #### REMOVED 100 FOR CHEMSETS
 for CIPK in 1 5 10 15 20; do
     RESPREFIX="cipsel2_$CIPK"
     STRING=" --cipselector_option 2 --cipselector_k $CIPK --resprefix $RESPREFIX"
@@ -95,10 +123,11 @@ for NORM in 0; do
 done
 
 ## Pareto Options: ###### REMOVED 'all' FOR CHEMSETS
-for PARETO in 'random' 'greedy' 'pareto_only' 'all' 'default'; do
-    RESPREFIX="pareto_$PARETO"
-    STRING=" --pareto_option $PARETO --resprefix $RESPREFIX"
-#    pass
+for PARETO in 'random' 'greedy' 'paretogreed' 'pareto_only' 'all' 'default'; do
+    RESPREFIX="res_pareto_$PARETO"
+#    STRING=" --pareto_option $PARETO --resprefix $RESPREFIX"
+    STRING=" --core_sizes 0 1 2 3 --pareto_option $PARETO --resprefix $RESPREFIX"
+#    reportchem
 done
 
 ## Contextsizes/Thickness: 
@@ -120,4 +149,35 @@ for SIZELIMITER in 0 1; do
     RESPREFIX="sizelimiter_$SIZELIMITER"
     STRING=" --graph_size_limiter $SIZELIMITER --resprefix $RESPREFIX"
 #    pass
+done
+
+
+## Cipselector Comparison Graphs
+KEEP=60
+#CIP0
+###for CIPK in 300 1500 3000 4500 6000 7500 9000; do # KEEP 30
+###for CIPK in 100 900 2250 3750 5250 6750 8250; do
+###for CIPK in 150 750 1500 2250 3000 3750 4500; do # KEEP 60
+for CIPK in 50 450 1125 1875 2625 3375 4125; do  
+    RESPREFIX="res_keep-$KEEP-cipsel0-$CIPK"
+    STRING=" --keepgraphs $KEEP --cipselector_option 0 --cipselector_k $CIPK --pareto_option 'greedy' --resprefix $RESPREFIX"
+    report
+done
+#CIP1
+###for CIPK in 10 50 100 150 200 250 300; do # KEEP 30
+###for CIPK in 50 30 75 125 175 225 275; do
+###for CIPK in 5 25 50 75 100 125 150; do # KEEP 60
+for CIPK in 1 15 38 63 88 113 138; do
+    RESPREFIX="res_keep-$KEEP-cipsel1-$CIPK"
+    STRING=" --keepgraphs $KEEP --cipselector_option 1 --cipselector_k $CIPK --pareto_option 'greedy' --resprefix $RESPREFIX"
+    report
+done
+#CIP2
+###for CIPK in 10 20 30 40 50 60 70 80 90 100; do # KEEP 30
+###for CIPK in 1 2 3 4 5 6 7 8 9; do
+###for CIPK in 5 10 15 20 25 30 35 40 45 50; do # KEEP 60
+for CIPK in 1 2 3 4 6 7 8 9; do
+    RESPREFIX="res_keep-$KEEP-cipsel2-$CIPK"
+    STRING=" --keepgraphs $KEEP --cipselector_option 2 --cipselector_k $CIPK --pareto_option 'greedy' --resprefix $RESPREFIX"
+    report
 done
